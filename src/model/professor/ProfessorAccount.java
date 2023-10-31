300d2e36dbc1f515ed008bd61dc3402de65ef534
Ram <chitrala.h@northeastern.edu>
2023-10-30 21:38:30 -0400

    change1test1

---
+++ src/model/professor/ProfessorAccount.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.professor;
+
+/**
+ *
+ * @author Donald Mucharla
+ */
+public class ProfessorAccount {
+    private String firstNamePro;
+    private String lastNamePro;
+    private String agePro;
+    private String countryPro;
+    private String languagePro;
+    private String bloodGroupPro;
+    private String username;
+    private String password;
+    private Professor professor;
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoginusername() {
+        return loginusername;
+    }
+
+    public void setLoginusername(String loginusername) {
+        this.loginusername = loginusername;
+    }
+
+    public String getLoginpassword() {
+        return loginpassword;
+    }
+
+    public void setLoginpassword(String loginpassword) {
+        this.loginpassword = loginpassword;
+    }
+    private String loginusername;
+    private String loginpassword;
+    public String getFirstNamePro() {
+        return firstNamePro;
+    }
+
+    public void setFirstNamePro(String firstNamePro) {
+        this.firstNamePro = firstNamePro;
+    }
+
+    public String getLastNamePro() {
+        return lastNamePro;
+    }
+
+    public void setLastNamePro(String lastNamePro) {
+        this.lastNamePro = lastNamePro;
+    }
+
+    public String getAgePro() {
+        return agePro;
+    }
+
+    public void setAgePro(String agePro) {
+        this.agePro = agePro;
+    }
+
+    public String getCountryPro() {
+        return countryPro;
+    }
+
+    public void setCountryPro(String countryPro) {
+        this.countryPro = countryPro;
+    }
+
+    public String getLanguagePro() {
+        return languagePro;
+    }
+
+    public void setLanguagePro(String languagePro) {
+        this.languagePro = languagePro;
+    }
+
+    public String getBloodGroupPro() {
+        return bloodGroupPro;
+    }
+
+    public void setBloodGroupPro(String bloodGroupPro) {
+        this.bloodGroupPro = bloodGroupPro;
+    }
+    
+    @Override
+    public String toString(){
+        
+        return firstNamePro+lastNamePro;
+        
+    }
+    
+         
+}
